26e15442f1e06e73cef5f687c70fda3b5be44db8
Amir Amiri <amiriamirr533@gmail.com>
2023-06-29 18:01:02 +0000

    copier commit

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,7 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="9hFVDMpbFVeLyT8TXZj0FoVjZVQYatNMtZnN6ZMNOwAy9B47DnSOymzwCXPA2aud",
+    default="Etc2rc5NaMIERD6dhtWv5J292IEFVFWDv3kDBrCqXrRbTN12T0PK6SSqBKnyeti2",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
--- config/settings/test.py
+++ config/settings/test.py
@@ -10,7 +10,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="qAZlzzl1SwBs65Y3IrbYSTKWGycyRUaoADHJFuZ6aELpP6i5jrRlbkvUtXP3uVZF",
+    default="1IucYuRkA2Fj9EuaerynRiuRuSTORdWExmAmB3dSqaKm5q8UtZHlsysKooeozYHr",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
