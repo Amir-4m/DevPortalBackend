e47ac1a5b8987bf51d9bcbdbbe2deafd627448bd
Amir Amiri <amiriamirr533@gmail.com>
2023-06-29 16:53:10 +0000

    copier commit

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,7 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="XcXQp643GDcPSIbfDLG7d1vDUDs3hr0hO0KS6hZVCq4cE8hNz7vDD6yw8eDSd6uR",
+    default="tunXfF1wzBGJOJRjGPvMlhMT4oX9Kf7f8plOCIhrHX7d48otH9j5N7Lbf9rOSDFE",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
