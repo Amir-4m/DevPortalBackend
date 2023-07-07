8676dfc49dc6501c049c18ddd3522d02d6978832
Amir Amiri <amiriamirr533@gmail.com>
2023-07-07 22:53:29 +0000

    cookiecutter-pp-backend preprocess commit

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,7 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="2rUie0oMuQ4zZygg7KW7vRQeSfOsBH80vtchcUgDxd7RPPZM4Ohmsqi3mvcpeptf",
+    default="EPyRz2HTUQqHbCMtSxqUG9uf9CAqSYplQwHBYIcpS0esxn5DIHpqDwkGM5TDUp0A",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
--- config/settings/test.py
+++ config/settings/test.py
@@ -10,7 +10,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="OP4fRtqkw5DVLJUqirHzK3Scd9csA6ypVH6tqeRUkzdbSmmbESN4AA0upqkr15CR",
+    default="dQc1VkDuv46n41AsVSqAxlfyrPsDa9QclLO3qVQYFrArOvtSyOSHpmaJRTkrtwNB",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
