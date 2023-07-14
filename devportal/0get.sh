e15bc591455aeccd2a8b41be68f5aa17e53f702f
Amir Amiri <amiriamirr533@gmail.com>
2023-07-14 08:57:23 +0000

    cookiecutter-pp-backend copier commit

---
+++ devportal/0get.sh
@@ -0,0 +1,30 @@
+DESTINATION="$1"
+ANSWER_FILE="$2"
+DATA="$3"
+
+
+if [ -z "$DESTINATION" ]; then
+	echo "DESTINATION is mandatory"
+	exit
+fi
+
+if [ -z "$ANSWER_FILE" ]; then
+	echo "ANSWER_FILE is mandatory"
+	exit
+fi
+
+if [ -z "$DATA" ]; then
+	echo "$DATA is mandatory"
+	exit
+fi
+echo "$DATA" | jq . > cookie-anwser.json
+PROJECT_SLUG = $(jq -r '.project_slug' $DATA)
+
+yes | cookiecutter --replay-file cookie-anwser.json https://github.com/projectpipeline/cookiecutter-django.git
+cp ./{{_copier_conf.answers_file}} ./cookiecutter_django/{{_copier_conf.answers_file}}.jinja
+cp ./copier.yml ./$PROJECT_SLUG/
+mv ./$PROJECT_SLUG/$PROJECT_SLUG/* ../pp_backend/{{project_name}}
+rm -r ./$PROJECT_SLUG/$PROJECT_SLUG
+copier copy --overwrite --UNSAFE -a $ANSWER_FILE ./$PROJECT_SLUG $DESTINATION
+
+#rm -r ./cookiecutter_django
