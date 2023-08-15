4e289017c9f903ebe6991f3ea066c9fa56baf711
Amir Amiri <amiriamirr533@gmail.com>
2023-08-15 02:15:41 +0000

    cookiecutter-pp-backend preprocess commit

--- config/settings/test.py
+++ config/settings/test.py
@@ -10,7 +10,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
-    default="B1ln7NZex3Ffvr9zDg9CsOuYeBOXMZ4Jf8pNWTFJoFfvWemAAASEb4eo1h8W5rnq",
+    default="oT9x0qmJjhwRhecrKokqC0sz3YDssOBftiZnaaw0Xp6GnRTm6Q0z77JZPlkeeugw",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
